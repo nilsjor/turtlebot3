d87c9a8859404809cd14c5eb11862a986fdc30c7
nilsjor <nilsjor@kth.se>
2024-09-26 11:54:02 +0200

    Add ping functionality

--- turtlebot3_support/script/ping_publisher.py
+++ turtlebot3_support/script/ping_publisher.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import rclpy
+import subprocess
 
 from geometry_msgs.msg import Vector3Stamped
 from rclpy.qos import QoSProfile
@@ -14,10 +15,28 @@ class PingPublisher(Node):
         super().__init__('ping_publisher')
         qos = QoSProfile(depth=10)
         self.pub = self.create_publisher(Vector3Stamped, 'ping', qos)
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.timer = self.create_timer(1/50, self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info('Ping publisher test')
+        result = subprocess.run(
+            ['ping', '-6', '-c', '1', '-W', '0.500', 'turtlebot'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        if result.returncode == 0:
+            output = result.stdout
+            time_ms = float(output.split('time=')[1].split(' ms')[0])
+        else:
+            time_ms = float('Inf')
+
+        msg = Vector3Stamped()
+        msg.header.frame_id = 'odom'
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.vector.z = time_ms
+        self.pub.publish(msg)
+        # self.get_logger().info(f'Published ping time: {time_ms} ms')
 
 def main():
     try:
