e7a2e99baaee9f9040fab2df876a92df2bafda8c
nilsjor <nilsjor@kth.se>
2025-03-04 11:15:27 +0100

    Add comments

--- turtlebot3_support/src/tf2_transform_node.cpp
+++ turtlebot3_support/src/tf2_transform_node.cpp
@@ -5,42 +5,50 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <memory>
 
+// Base class for handling different message types
 class TransformHandler
 {
 public:
+    // Pure virtual function to subscribe to a topic and set up the transformation
     virtual void subscribe(rclcpp::Node::SharedPtr node, const std::string& input_topic, const std::string& output_topic,
                            const std::string& target_frame, const tf2_ros::Buffer& tf_buffer) = 0;
     virtual ~TransformHandler() = default;
 };
 
+// Derived class for handling geometry_msgs::msg::PointStamped messages
 class PointStampedHandler : public TransformHandler
 {
 public:
     void subscribe(rclcpp::Node::SharedPtr node, const std::string& input_topic, const std::string& output_topic,
                    const std::string& target_frame, const tf2_ros::Buffer& tf_buffer) override
     {
-
+        // Subscribe to the input topic
         subscription_ = node->create_subscription<geometry_msgs::msg::PointStamped>(
             input_topic, 10, 
             [this, target_frame, &tf_buffer](const geometry_msgs::msg::PointStamped::SharedPtr msg) {
                 this->callback(msg, target_frame, tf_buffer);
             });
 
+        // Create a publisher for the transformed messages
         publisher_ = node->create_publisher<geometry_msgs::msg::PointStamped>(output_topic, 10);
         RCLCPP_INFO(node->get_logger(), "Transforming points from '%s' to frame '%s' and publishing on '%s'.",
                     input_topic.c_str(), target_frame.c_str(), output_topic.c_str());
     }
 
 private:
+    // Callback function to handle incoming messages and perform the transformation
     void callback(const geometry_msgs::msg::PointStamped::SharedPtr msg, 
                   const std::string& target_frame, const tf2_ros::Buffer& tf_buffer)
     {
         try
         {
+            // Look up the transform from the source frame to the target frame
             geometry_msgs::msg::TransformStamped transform = tf_buffer.lookupTransform(
                 target_frame, msg->header.frame_id, tf2::TimePointZero);
+            // Transform the message
             geometry_msgs::msg::PointStamped transformed_msg;
             tf2::doTransform(*msg, transformed_msg, transform);
+            // Publish the transformed message
             publisher_->publish(transformed_msg);
         }
         catch (const tf2::TransformException &ex)
@@ -53,12 +61,14 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr publisher_;
 };
 
+// Factory function to create the appropriate handler based on the message type
 std::shared_ptr<TransformHandler> create_handler(const std::string& type)
 {
     if (type == "geometry_msgs/msg/PointStamped")
     {
         return std::make_shared<PointStampedHandler>();
     }
+    // Add more conditions here to handle other message types
     return nullptr;
 }
 
@@ -77,6 +87,7 @@ int main(int argc, char **argv)
     std::string target_frame = argv[3];
     std::string output_topic = (argc > 4) ? argv[4] : (input_topic + "_" + target_frame);
 
+    // Create the appropriate handler based on the message type
     auto handler = create_handler(type);
     if (!handler)
     {
@@ -88,6 +99,7 @@ int main(int argc, char **argv)
     tf2_ros::Buffer tf_buffer(node->get_clock());
     tf2_ros::TransformListener tf_listener(tf_buffer);
 
+    // Subscribe to the input topic and set up the transformation
     handler->subscribe(node, input_topic, output_topic, target_frame, tf_buffer);
 
     rclcpp::spin(node);
